fd7d6d761567daf01bf82945b0fca274d66270e5
Ryan Shao <ryan.shao95@gmail.com>
2020-07-29 22:21:33 -0400

    js added subcategory display

--- scripts/script.js
+++ scripts/script.js
@@ -6,31 +6,98 @@
 // pass the variable to a getDrinks function 
 // in function getDrinks, call the api and pass in the data of the users sub-category selection
 
-<<<<<<< HEAD
-const cocktailApp = {
-    party :[ 
-        milk = {
-            idDrink
-        }
-        
-        float: [
-
-        ]
-    ]
-}
-=======
 const cocktailApp = {};
->>>>>>> 6d62d5a3fd37113e0bc39f9d922bda1cd5a3826f
 
-const drinkCategory = {
-    party: ['shot', 'milk / float / shake', 'punch / party drink'],
-    chill: ['cocktail', 'beer', 'coffee / cocoa'],
-    random: ['']
+cocktailApp.party = {
+    shot: [
+        {
+            title: 'shot',
+            imageURL: 'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/shots.jpg);'
+        }
+    ],
+    punch: [
+        {
+            title: 'punch / party drink',
+            imageUrl: "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/punch.jpg);"
+        }
+    ],
+    float: [
+      {
+        title: 'float',
+        imageURL: 'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/float.jpg);'
+      }
+    ],
+    chill: [
+        {
+            title: 'cocktail', 
+            imageURL: "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/cocktail.jpg);",
+        },
+        {
+            title: 'beer',
+            imageURL: "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/beer.jpg);",
+        },
+        {
+            title: 'coffee / cocoa',
+            imageURL: "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(../../assets/coffee.jpg);",
+        }
+    ],
+
+    random: [
+        {
+            key: "",
+        }
+    ],
 }
 
-<<<<<<< HEAD
-// function ready
-=======
+//DISPLAY SUBCATEGORY BASED OFF OF USER CATEGORY SELECTION
+//DOC READY FUNCTION
+$(function() {
+    function randomItem(optionsArray) {
+		const index = Math.floor(Math.random() * optionsArray.length);
+		return optionsArray[index];
+    }
+
+     //Force scroll position to top at page refresh
+    $('html, body').animate({scrollTop: 0}, 100);
+
+    //Slow scroll from start to first slide
+    $('.start').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $(".questions").offset().top
+        }, 
+        'slow'
+        );
+    });
+
+    //USER CLICKS READY
+    cocktailApp.getSelection = function(){
+    $('.form').on('start', function(event){
+        event.preventDefault();
+        //Figure out which category user chooses
+        const mood = $('input[name=mood]:checked').val();
+        console.log(mood);
+        const userMood = cocktailApp.party[mood];
+
+        //Dependent on category chosen, display images on subcategories
+        if (userMood === 'party'){
+            $(".category1").css("background", userMood[party].imageURL) && 
+            $(".category2").css("background", userMood[party].imageURL) && 
+            $(".category3").css("background", userMood[party].imageURL)
+        } else if (userMood === 'chill'){
+            $(".category1").css("background", userMood[chill].imageURL) && 
+            $(".category2").css("background", userMood[chill].imageURL) && 
+            $(".category3").css("background", userMood[chill].imageURL)
+        } else if (userMood === 'surprise'){
+            $('html, body').animate({
+                scrollBot: $("footer").offset().top
+            }, 
+            'slow');
+        }
+    })
+};   
+    cocktailApp.init();
+})
+
 cocktailApp.getDrinks = function() {
         
     $.ajax({
@@ -79,19 +146,11 @@ cocktailApp.showResults = function(drink) {
     })
 }
 
->>>>>>> 6d62d5a3fd37113e0bc39f9d922bda1cd5a3826f
-$(function() {
-    cocktailApp.getDrinks();
 
-<<<<<<< HEAD
-    cocktailApp.init();
-    
-})
 
 cocktailApp.init = function (){
-
+    cocktailApp.getSelection();
+    cocktailApp.getDrinks();
 }
 
-=======
-})
->>>>>>> 6d62d5a3fd37113e0bc39f9d922bda1cd5a3826f
+
